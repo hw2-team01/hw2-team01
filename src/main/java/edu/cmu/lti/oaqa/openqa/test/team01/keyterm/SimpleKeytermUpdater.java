16815d4d558ae8050deecd639fd4f127acf4bce6
Victor Chahuneau <vchahune@cs.cmu.edu>
2012-12-05 15:37:35 -0500

    Added SimpleKeytermUpdater (fixes #12)

---
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team01/keyterm/SimpleKeytermUpdater.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2012 Carnegie Mellon University
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package edu.cmu.lti.oaqa.openqa.test.team01.keyterm;
+
+
+import java.util.List;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermUpdater;
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
+
+public class SimpleKeytermUpdater extends AbstractKeytermUpdater {
+
+  @Override
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    super.initialize(aContext);
+  }
+
+  @Override
+  protected List<Keyterm> updateKeyterms(String question, List<Keyterm> keyterms) {
+    question = question.replace('?', ' ');
+    question = question.replace('(', ' ');
+    question = question.replace('[', ' ');
+    question = question.replace(')', ' ');
+    question = question.replace(']', ' ');
+    question = question.replace('/', ' ');
+    question = question.replace('\'', ' ');
+
+    String[] questionTokens = question.split("\\s+");
+    for (int i = 0; i < questionTokens.length; i++) {
+      keyterms.add(new Keyterm(questionTokens[i]));
+    }
+
+    return keyterms;
+  }
+}
